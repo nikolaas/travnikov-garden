79b4179c2e15004e7b48bf204e40ac608cc36b9d
Nikolay Stupak <nikolaystupak@gmail.com>
2017-03-21 00:32:15 +0300

    add font's config

--- webpack.config.js
+++ webpack.config.js
@@ -102,6 +102,16 @@ const common = {
                         }
                     ]
                 })
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '../fonts/[name].[ext]',
+                        outputPath: 'fonts/'
+                    }
+                }
             }
         ]
     },
