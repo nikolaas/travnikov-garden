2a10e77f3d7441975725d6d56722d836183a4993
Nikolay Stupak <nikolaystupak@gmail.com>
2017-04-01 17:52:35 +0300

    Dynamic generation of the slider

---
+++ src/pages/index.data.js
@@ -0,0 +1,23 @@
+import slide1 from '../resources/indi-images/slide1.png';
+import slide2 from '../resources/indi-images/slide2.png';
+import slide3 from '../resources/indi-images/slide3.png';
+
+const sliderItems = [
+    {
+        src: slide1,
+        title: 'First slide',
+        description: 'First slide description'
+    },
+    {
+        src: slide2,
+        title: 'Second slide',
+        description: 'Second slide description'
+    },
+    {
+        src: slide3,
+        title: 'Third slide',
+        description: 'Third slide description'
+    }
+];
+
+export default {sliderItems};
