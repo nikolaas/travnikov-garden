f335b2a4a3cfa17d7df2f39c34281eb01786469f
Nikolay Stupak <nikolaystupak@gmail.com>
2017-04-04 19:39:57 +0300

    main section title extracted to component

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
-import './header'
-import './navigation'
-import './popup-menu'
-import './slider'
-import './news'
-import './news-subcscription'
+import './header';
+import './navigation';
+import './main-section-title';
+import './popup-menu';
+import './slider';
+import './news';
+import './news-subcscription';
