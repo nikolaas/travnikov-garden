14098f9f6df87fb49d73e6d471a99c3c0e2364c9
Nikolay Stupak <nikolaystupak@gmail.com>
2017-05-17 02:47:49 +0300

    добавлен компонент garden-statistics

--- src/components/index.js
+++ src/components/index.js
@@ -9,6 +9,7 @@ import './sidebar';
 import './news';
 import './news-subcscription';
 import './garden-about';
+import './garden-statistics';
 import './ours-plants';
 import './feedback-form';
 import './map';
