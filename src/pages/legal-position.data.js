60d8be872f0e93bb42dfd01e0aa95a209fdeedd2
Nikolay Stupak <nikolaystupak@gmail.com>
2017-05-17 15:06:18 +0300

    добавлен новый объект documents для отображения документов на странице legal-position

--- src/pages/legal-position.data.js
+++ src/pages/legal-position.data.js
@@ -14,4 +14,26 @@ const sidebar = {
     }
 };
 
-export {breadcrumbs, sidebar};
+
+const documents = [
+    {
+        image: require("../resources/indi-images/doc-prew1.jpg"),
+        name: 'Паспорт 1989',
+        href:"doc1",
+        data: '6,22 МБ'
+    },
+    {
+        image: require("../resources/indi-images/doc-prew2.jpg"),
+        name: 'Основание придания статуса ООПТ',
+        href:"doc2",
+        data: '49,3 МБ'
+    },
+    {
+        image: require("../resources/indi-images/doc-prew3.jpg"),
+        name: 'Паспорт 2016',
+        href:"doc3",
+        data: '385 КБ'
+    }
+];
+
+export {breadcrumbs, sidebar, documents};
