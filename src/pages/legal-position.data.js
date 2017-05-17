f96479154726f9eea62b4636e28a0a12ad59b3cd
Nikolay Stupak <nikolaystupak@gmail.com>
2017-05-17 04:03:38 +0300

    добавлены зачатки страницы "Юридическое положение"

---
+++ src/pages/legal-position.data.js
@@ -0,0 +1,17 @@
+const breadcrumbs = [
+    {name:"Главная", href:"/"},
+    {name:"О саде", href:"/about"},
+    {name:"Юридическое положение"}
+];
+
+const sidebar = {
+    menu: {
+        title: 'О саде',
+        items: [
+            {href: '', name: 'История сада'},
+            {href: '', name: 'Юридическое положение'}
+        ]
+    }
+};
+
+export {breadcrumbs, sidebar};
