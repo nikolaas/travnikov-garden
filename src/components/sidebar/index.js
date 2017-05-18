ab57f22289194c30a1e12a7a6ac1ecba57dcd587
Nikolay Stupak <nikolaystupak@gmail.com>
2017-05-18 19:42:55 +0300

    В сайдбар добавлен раздел "Подписка на новости"

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,6 +1,7 @@
 import './sidebar-menu';
 import './sidebar-tags';
 import './sidebar-last-news';
+import './sidebar-news-subscription';
 import './sidebar-event-calender';
 import './sidebar-social';
 import './sidebar.styl';
