0bfa19b23c63bef14388383ffe7a0bcc9c425aee
Nikolay Stupak <nikolaystupak@gmail.com>
2017-06-03 16:03:16 +0300

    доработана структура внешних картинок сайта

--- src/pages/index/index.data.js
+++ src/pages/index/index.data.js
@@ -1,32 +1,16 @@
-import slide1 from '../../resources/external-images/slide1.png';
-import slide2 from '../../resources/external-images/slide2.png';
-import slide3 from '../../resources/external-images/slide3.png';
-
-import news1 from '../../resources/external-images/new1.png';
-import news3 from '../../resources/external-images/new3.png';
-import news2 from '../../resources/external-images/new2.png';
-
-import oursPlant1 from '../../resources/external-images/ours-plant-1.png';
-import oursPlant2 from '../../resources/external-images/ours-plant-2.png';
-import oursPlant3 from '../../resources/external-images/ours-plant-3.png';
-import oursPlant4 from '../../resources/external-images/ours-plant-4.png';
-import oursPlant5 from '../../resources/external-images/ours-plant-5.png';
-import oursPlant6 from '../../resources/external-images/ours-plant-6.png';
-import oursPlant7 from '../../resources/external-images/ours-plant-7.png';
-
 const sliderItems = [
     {
-        src: slide1,
+        src: require('../../resources/external-images/index/slide1.png'),
         title: 'Ботанический сад П. И. Травникова',
         description: 'Основан 1957-1958 гг. на Фрунзенской набережной Павлом Ивановичем Травниковым'
     },
     {
-        src: slide2,
+        src: require('../../resources/external-images/index/slide2.png'),
         title: 'Ботанический сад П. И. Травникова',
         description: 'Основан 1957-1958 гг. на Фрунзенской набережной Павлом Ивановичем Травниковым'
     },
     {
-        src: slide3,
+        src: require('../../resources/external-images/index/slide3.png'),
         title: 'Ботанический сад П. И. Травникова',
         description: 'Основан 1957-1958 гг. на Фрунзенской набережной Павлом Ивановичем Травниковым'
     }
@@ -38,35 +22,35 @@ const newsItems = [
         month: 'Июн',
         title: 'Заголовок новости',
         shortContent: 'Семейство конскокаштановых. Занесено в красную книгу... Семейство конскокаштановых. Занесено в красную книгу... Семейство конскокаштановых. Занесено в красную книгу... Семейство конскокаштановых. Занесено в красную книгу...',
-        image: news1
+        image: require('../../resources/external-images/index/new1.png')
     },
     {
         day: '19',
         month: 'Окт',
         title: 'Заголовок новости',
         shortContent: 'Семейство кипарисовые. Занесено в красную книгу...',
-        image: news2
+        image: require('../../resources/external-images/index/new2.png')
     },
     {
         day: '2',
         month: 'Дек',
         title: 'Заголовок новости',
         shortContent: 'Семейство лилейные. Занесено в красную книгу...',
-        image: news3
+        image: require('../../resources/external-images/index/new3.png')
     }
 ];
 
 const oursPlants = [
-    {image: oursPlant1},
-    {image: oursPlant2},
-    {image: oursPlant3},
-    {image: oursPlant4},
-    {image: oursPlant5},
-    {image: oursPlant4},
-    {image: oursPlant5},
-    {image: oursPlant6},
-    {image: oursPlant1},
-    {image: oursPlant2}
+    {image: require('../../resources/external-images/index/ours-plant-1.png')},
+    {image: require('../../resources/external-images/index/ours-plant-2.png')},
+    {image: require('../../resources/external-images/index/ours-plant-3.png')},
+    {image: require('../../resources/external-images/index/ours-plant-4.png')},
+    {image: require('../../resources/external-images/index/ours-plant-5.png')},
+    {image: require('../../resources/external-images/index/ours-plant-4.png')},
+    {image: require('../../resources/external-images/index/ours-plant-5.png')},
+    {image: require('../../resources/external-images/index/ours-plant-6.png')},
+    {image: require('../../resources/external-images/index/ours-plant-1.png')},
+    {image: require('../../resources/external-images/index/ours-plant-2.png')}
 ];
 
 export default {sliderItems, newsItems, oursPlants};
